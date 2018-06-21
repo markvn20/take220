c25d4159ac4f9c4076752ee773bd10a24ef2a96f
markvn20 <mnguye17@asu.edu>
2018-06-21 05:32:10 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,30 +1,52 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route, Switch } from 'react-router-dom'
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import About from './components/About';
-import Services from './components/Services';
-import Contact from './components/Contact';
+import image from './image.jpeg';
+import image2 from './image2.jpeg';
 
 
-
+var count = 100;
+var amount = 100
 class App extends Component {
+  state = {
+    transform: 'translate(0%)',
+
+  }
+
+  left = () => {
+    if(count > 2) {
+      count = 0;
+      amount = 0
+    }
+    count+=1
+    amount-=100
+    console.log(count)
+    this.setState({
+      transform: `translate(${amount}%)`,
+    })
+  }
+
+  right = () => {
+    if(count > 5) {
+      count = 0;
+    }
+    count+=1
+    amount+=100
+    console.log(count)
+    this.setState({
+      transform: `translate(${amount}%)`,
+    })
+  }
+
   render() {
     return (
       <div className="App">
-    	<BrowserRouter>
-    		<div>
-	    		<Header/>
-	    		<Switch>
-		      		<Route path="/" component={Home} exact />
-		      		<Route path="/about" component={About} />
-		      		<Route path="/services" component={Services} />
-		      		<Route path="/contact" component={Contact} />
-	      		</Switch>
-      		</div>
-      	</BrowserRouter>
-        <Footer/>
+        <h1 onClick={this.left}>LEFT</h1>
+        <h1 onClick={this.right}>right</h1>
+        <div className="sliderContainer"  style={this.state}>
+          <div className="slider1 slider"><img src={image} /></div>
+          <div className="slider2 slider"><img src={image2} /></div>
+          <div className="slider3 slider"><img src={image} /></div>
+          <div className="slider4 slider"><img src={image2} /></div>
+        </div>
       </div>
     );
   }
